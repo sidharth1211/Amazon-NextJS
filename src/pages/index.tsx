9810d3fb1d7afe2358537491e3dee398a3d478d7
Sidharth Chaudhary <communicate.sidharth@gmail.com>
2023-08-28 17:57:01 +0530

    added carousel and products feed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,35 @@
 import Head from "next/head";
 import Header from "../components/Header";
-export default function Home() {
+import Banner from "../components/Banner";
+import ProductFeed from "../components/ProductFeed";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+export default function Home({ products }) {
   return (
-    <div>
+    <div className="bg-gray-100 h-screen ">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
-      
+
       {/* Header */}
       <Header />
+
+      <main className="max-w-screen-xl mx-auto">
+        {/* Banner */}
+        <Banner />
+        {/* Product Feed */}
+        <ProductFeed products={products}/>
+      </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  );
+  return {
+    props: {
+      products,
+    },
+  };
 }
